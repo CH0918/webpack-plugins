aa8f6b9db54710923e4ab8058ccb9cc829241991
huangdongjiang <dongjiang.huang@get88.cn>
2022-12-02 21:22:42 +0800

    js 打包

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const path = require('path');
 const DonePlugin = require('./plugins/DonePlugin');
+const CompressAssetsPlugin = require('./plugins/CompressAssetsPlugin');
+const output = path.join(__dirname, './dist');
 module.exports = {
   entry: path.resolve(__dirname, './index.js'),
   output: {
     filename: '[name].js',
-    path: path.join(__dirname, './dist'),
+    path: output,
   },
-  plugins: [new DonePlugin()],
+  plugins: [new CompressAssetsPlugin({ output: 'zip' })],
 };
